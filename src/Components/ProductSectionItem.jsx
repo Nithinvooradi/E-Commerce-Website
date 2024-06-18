ba44961539c7e392a0a3aa271f9106bc3885efa3
Nithinvooradi <vooradinithin@gmail.com>
2024-06-18 12:23:53 +0530

    small refactor

--- src/Components/ProductSectionItem.jsx
+++ src/Components/ProductSectionItem.jsx
@@ -18,7 +18,9 @@ const ProductSectionItem = ({
   return (
     <div>
       <div className=" relative w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mt-4">
-        <p className="text-red-700 text-base font-inter absolute top-12 right-8 z-[10] rotate-45 font-medium">SALE%</p>
+        <p className="text-red-700 text-base font-inter absolute top-12 right-8 z-[10] rotate-45 font-medium">
+          SALE%
+        </p>
         <div>
           <img className="p-6 h-70 " src={img} alt="product" />
         </div>
@@ -31,7 +33,8 @@ const ProductSectionItem = ({
 
           <div className="flex items-center justify-between mt-4">
             <span className="text-base font-bold text-gray-900 dark:text-white">
-              <span className="text-red-800">Size Left : </span>{defaultSize}
+              <span className="text-red-800">Size Left : </span>
+              {defaultSize}
             </span>
             <span className="text-base font-bold text-gray-900 dark:text-white">
               Color:{" "}
@@ -44,7 +47,7 @@ const ProductSectionItem = ({
           <div className=" flex justify-between items-center mt-4  border-t-2 p-2">
             <h2 className="font-inter font-bold text-xl">{price}$</h2>
             <button
-              className="text-xl border rounded-lg px-6 py-2 w-[70%] hover:bg-blue-800 hover:text-white "
+              className="text-xl border border-blue-800 rounded-lg px-6 py-2 w-[70%] hover:bg-blue-800 hover:text-white "
               onClick={() =>
                 dispatch(
                   addToCart({
