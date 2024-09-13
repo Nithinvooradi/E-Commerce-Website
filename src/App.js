a8c229059a4ec4449778afe5d1984d398b49ab93
Vooradi Nithin <87109401+Nithinvooradi@users.noreply.github.com>
2024-09-13 14:05:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,19 +3,18 @@ import FilteredProducts from "./Components/FilteredProducts";
 import Main from "./Components/Main";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SingleProduct from "./Components/SingleProduct";
-import { useSelector } from "react-redux";
+
 import Login from "./Components/Login";
 
 function App() {
   
-  const user = useSelector((state) => state.auth.user);
-  const {authUser} = user;
+ 
 
   return (
     <div className="App text-3xl">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={authUser ? <Main /> : <Login/>} />
+          <Route path="/" element={<Main />} />
           
 
           <Route
